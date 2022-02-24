9b90701a63ce9d4c503cd084df98067fa190091d
Shane O'Moore <shaneiadt@gmail.com>
2022-02-24 15:32:41 +0000

    diplay options

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+
+import Option from './Option/Option';
 
 function Sidebar() {
   return (
@@ -16,6 +26,16 @@ function Sidebar() {
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
+
+      <Option Icon={InsertCommentIcon} title="Threads"/>
+      <Option Icon={InboxIcon} title="Mentions & Reactions"/>
+      <Option Icon={DraftsIcon} title="Saved Items"/>
+      <Option Icon={BookmarkBorderIcon} title="Channel Browser"/>
+      <Option Icon={PeopleAltIcon} title="People & User Groups"/>
+      <Option Icon={AppsIcon} title="Apps"/>
+      <Option Icon={FileCopyIcon} title="File Browser"/>
+      <Option Icon={ExpandLessIcon} title="Show Less"/>
+      
     </SidebarContainer>
   )
 }
