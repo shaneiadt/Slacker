169251ae2a9142e5d1faf29e9453ca3c1049c3a4
Shane O'Moore <shaneiadt@gmail.com>
2022-02-24 15:22:15 +0000

    use sidebar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import Header from '../Header/Header';
+import styled from 'styled-components';
 
+import Header from '../Header/Header';
+import Chat from '../Chat/Chat';
+import Sidebar from '../Sidebar/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Header />} />
-    </Routes>
+      <Header />
+      <AppBody>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </AppBody>
     </>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
