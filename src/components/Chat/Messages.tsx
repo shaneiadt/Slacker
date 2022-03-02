fca891a67932e7c0111e09274c65971879886f97
Shane O'Moore <shaneiadt@gmail.com>
2022-03-02 11:29:18 +0000

    scroll to bottom ref component

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -1,11 +1,17 @@
 import { collection, getFirestore, orderBy, query } from 'firebase/firestore';
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
 import { app } from '../../firebase';
 
 const Messages: React.FC<{ roomId: string }> = ({ roomId }) => {
     const [messages] = useCollection(query(collection(getFirestore(app), 'rooms', roomId, 'messages'), orderBy('timestamp', 'asc')));
+    const chatBottomRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        chatBottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [messages]);
+
 
     const formatDate = (timestamp: { toDate: () => Date }) => {
         if (!timestamp) return '';
@@ -27,14 +33,14 @@ const Messages: React.FC<{ roomId: string }> = ({ roomId }) => {
                     </MessageInfo>
                 </MessageContainer>
             ))}
+            <ChatBottom ref={chatBottomRef} />
         </MessagesContainer>
     )
 }
 
 export default Messages;
 
-const MessagesContainer = styled.div`
-`;
+const MessagesContainer = styled.div``;
 
 const MessageContainer = styled.div`
     display: flex;
@@ -56,4 +62,8 @@ const MessageInfo = styled.div`
         margin-left: 4px;
         font-size: 10px;
     }
+`;
+
+const ChatBottom = styled.div`
+    padding: 50px;
 `;
